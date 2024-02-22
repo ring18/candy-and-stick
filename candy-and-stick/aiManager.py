047b3bae1c70edeef50a720694eeb2582ba1b88d
YukiSakai <vmcsamyuki0227@gmail.com>
2024-02-22 16:16:26 +0900

    merge Controller and AI Manager

--- candy-and-stick/aiManager.py
+++ candy-and-stick/aiManager.py
@@ -1,8 +1,10 @@
-import time
+
 import cv2 as cv
 import matplotlib.pyplot as plt
 import picamera2 as picamera
 import keyboard
+import os
+from pathlib import Path
 	
 
 class ManagementAI:
@@ -13,16 +15,18 @@ class ManagementAI:
     def Start(self):
         self.camera.resolution = (512, 384)
         self.camera.start_preview()
+        if not os.path.exists("img"):
+            os.makedirs("img")
 
     def GetImage(self):
-        fn = 'my_pic.jpg'
+        fn = os.path.join('img', 'my_pic.jpg')
         self.camera.capture(fn)
         return fn
     
     def InputImagetoAI(self, fn):
         img = cv.imread(fn)
         grayimg = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-        eye_cascade = cv.CascadeClassifier('/home/forpbl/workspace/haarcascade_eye.xml')
+        eye_cascade = cv.CascadeClassifier(os.path.join(Path(os.getcwd()).parent, 'haarcascade_eye.xml'))
         eyerect = eye_cascade.detectMultiScale(grayimg)
         if len(eyerect) != 2:
             print("0")
@@ -41,9 +45,11 @@ class ManagementAI:
             self.results.append(result)
             time.sleep(time_span)
 
+'''
 ai = ManagementAI()
 ai.Start()
 time.sleep(1) #これはカメラ初期化の時間
 ai.Run(1)  # 1秒ごとに処理を繰り返す
+'''
 
     
